862970cd1ecd08b2642cb4f0bf9aa512b181715b
propro1027 <for.pro.business.address@gmail.com>
2021-01-01 13:07:25 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'static_pages#top'
+  
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   get '/signup' ,to: 'users#new'
  
 end
